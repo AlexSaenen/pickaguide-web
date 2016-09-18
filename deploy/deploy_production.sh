d1d16d8ef146ba2264c6928c7cee3660ee0377f0
AlexSaenen <alexander.saenen@epitech.eu>
2016-09-18 19:33:08 +0200

    Final fix for deploy scripts

--- deploy/deploy_production.sh
+++ deploy/deploy_production.sh
@@ -1,4 +1,5 @@
 sudo service docker start
+docker login -u "pickaguidedockercloud" -p "lucasanstoast6"
 make build push
 
-ssh root@82.223.82.41 "deploy-web-production"
+ssh root@82.223.82.41 "bash -ic deploy-web-production"
