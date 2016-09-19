53be91fb0778a3e11568ddb620c898b63d88f2a8
AlexSaenen <alexander.saenen@epitech.eu>
2016-09-19 09:44:33 +0200

    Trying a more clean approach to deploy

--- deploy/deploy_production.sh
+++ deploy/deploy_production.sh
@@ -1,5 +1,5 @@
-sudo service docker start
-echo "alexander.saenen@epitech.eu" | docker login -u "pickaguidedockercloud" -p "lucasanstoast6"
-sudo make build push
+# sudo service docker start
+# echo "alexander.saenen@epitech.eu" | docker login -u "pickaguidedockercloud" -p "lucasanstoast6"
+# make build push
 
 ssh root@82.223.82.41 "bash -ic deploy-web-production"
