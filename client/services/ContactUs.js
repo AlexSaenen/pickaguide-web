ef2f46e4d9b7abd0869a6eed77d9c1d0bdf5afd1
vividy <vivien.pradelles@gmail.com>
2016-11-18 16:05:28 -0500

    creation de la page contactUs empty

---
+++ client/services/ContactUs.js
@@ -0,0 +1,21 @@
+import ContactUsActions from '../actions/ContactUs.js';
+import PromiseApi from './promiseApi.js';
+
+export default class ContactUsApi {
+    static getContactUs() {
+        //console.log('Form signin:', form);
+//        PromiseApi.post('/account/signup', form) // route a modifier plus tard POST
+        PromiseApi.get('/contactus') // route a modifier plus tard GET
+        .then((result) => {
+            if (result.error) {
+                ContactUsActions.requestContactUsError(result.error);
+                return;
+            }
+
+            ContactUsActions.requestContactUsSuccess(result);
+        })
+        .catch((err) => {
+            ContactUsActions.requestContactUsError(err);
+        });
+    }
+}
--- client/stores/User.js
+++ client/stores/User.js
@@ -1,28 +1,21 @@
 import alt from '../alt';
+
 import SigninActions from '../actions/Signin.js';
 import LoginActions from '../actions/Login.js';
+import ContactUsActions from '../actions/ContactUs.js';
+
 import SigninApi from '../services/Signin.js';
 import LoginApi from '../services/Login.js';
+import ContactUsApi from '../services/ContactUs.js';
+
 
 class UserStore {
     constructor() {
         this.message = '';
         this.code = 0;
-        this.profile = {
-          'nom' : "Sangoï",
-          'prenom' : "Lucas",
-          'DateDeNaissance' : "10 janvier 1908",
-          'Sexe' : "Masculin",
-          'email' : "lucas.sangoi@epitech.eu",
-          'numTel' : "0670628885",
-          'Ville' : "Canterbury",
-          'DescriptionPerso' : "Salut, je m'appelle Lucas, je viens de Toulouse, je suis actuellement en 4ème année à EPITECH, une école d'informatique. Ce site fait parti d'un projet de fin d'étude dans le cadre de mon école, j'espère que vous appricirez votre expérience sur notre site !",
-          'CentreDinteret' : "Football, informatique, sortir",
-          'photo' : "https://cdn.local.epitech.eu/userprofil/profilview/lucas.sangoi.jpg",
-        }
-
         this.bindActions(SigninActions);
         this.bindActions(LoginActions);
+        this.bindActions(ContactUsActions);
     }
 
     onRequestSignin(form) {
@@ -56,6 +49,51 @@ class UserStore {
     onRequestLoginError(error) {
         this.error = error;
     }
+
+    onRequestContactUs() {
+      console.log('Try to contact API')
+        ContactUsApi.getContactUs();
+    }
+
+    onRequestContactUsSuccess(profileInfo) {
+        // this.error = null;
+        console.log('SUC', profileInfo);
+//        this.message = profileInfo.result;
+        this.message = {
+          'nom' : "Sangoï",
+          'prenom' : "Lucas",
+          'DateDeNaissance' : "10 janvier 1908",
+          'Sexe' : "Masculin",
+          'email' : "lucas.sangoi@epitech.eu",
+          'numTel' : "0670628885",
+          'Ville' : "Canterbury",
+          'DescriptionPerso' : "Salut, je m'appelle Lucas, je viens de Toulouse, je suis actuellement en 4ème année à EPITECH, une école d'informatique. Ce site fait parti d'un projet de fin d'étude dans le cadre de mon école, j'espère que vous appricirez votre expérience sur notre site !",
+          'CentreDinteret' : "Football, informatique, sortir",
+          'photo' : "https://cdn.local.epitech.eu/userprofil/profilview/lucas.sangoi.jpg",
+        };
+        this.code = 200;
+    }
+
+    onRequestContactUsError(error) {
+      console.log('ERR', error);
+      //this.message = JSON.stringify(error.response.body) !== undefined ? JSON.stringify(error.response.body.err) : JSON.stringify(error);
+      // this.message = JSON.stringify(error);
+      // this.message = "SUCCESS";
+      this.message = {
+          'nom' : "Sangoï",
+          'prenom' : "Lucas",
+          'DateDeNaissance' : "10 janvier 1908",
+          'Sexe' : "Masculin",
+          'email' : "lucas.sangoi@epitech.eu",
+          'numTel' : "0670628885",
+          'Ville' : "Canterbury",
+          'DescriptionPerso' : "Salut, je m'appelle Lucas, je viens de Toulouse, je suis actuellement en 4ème année à EPITECH, une école d'informatique. Ce site fait parti d'un projet de fin d'étude dans le cadre de mon école, j'espère que vous appricirez votre expérience sur notre site !",
+          'CentreDinteret' : "Football, informatique, sortir",
+          'photo' : "https://cdn.local.epitech.eu/userprofil/profilview/lucas.sangoi.jpg",
+        };
+      this.code = 400;
+    }
+
 }
 
 export default alt.createStore(UserStore, 'UserStore');
