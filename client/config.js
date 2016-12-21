ef8caebc15c93fca68c81db93a5adb167e644214
AlexSaenen <alexander.saenen@epitech.eu>
2016-12-21 14:28:02 -0800

    Trying other log

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 function getApiUrl() {
-    console.log('ENVIRONMENT ', process.env.ENVIRONMENT);
+    console.log('ENVIRONMENT ', process.env.ENVIRONMENT, process.env.NODE_ENV);
     if (process.env.ENVIRONMENT === 'production') {
         return 'http://82.223.82.41:3000';
     } else if (process.env.ENVIRONMENT === 'staging') {
