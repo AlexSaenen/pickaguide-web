2ea949a9682933094ec0f7ef21d1f6b3db337deb
vivien pradelles <vivien pradelles>
2017-01-19 13:00:36 -0800

    comment test

--- tests/first-test.js
+++ tests/first-test.js
@@ -1,27 +1,27 @@
 describe('Tests on client web', function() {
-  it('complete complete form', function() {
-  	browser.ignoreSynchronization = true;
-		browser.get('http://localhost:5000');
+ //  it('complete complete form', function() {
+ //  	browser.ignoreSynchronization = true;
+	// 	browser.get('http://localhost:5000');
 
-		element(by.css('[href="/contactus"]')).click();
+	// 	element(by.css('[href="/contactus"]')).click();
 
-		element(by.name('name')).sendKeys("testProtractor");
-		element(by.name('email')).sendKeys("test@test.com");
-		element(by.name('phone')).sendKeys("+1 514 560 3976");
-		element(by.name('message')).sendKeys("Bonjour, ceci est un test de protractor");
+	// 	element(by.name('name')).sendKeys("testProtractor");
+	// 	element(by.name('email')).sendKeys("test@test.com");
+	// 	element(by.name('phone')).sendKeys("+1 514 560 3976");
+	// 	element(by.name('message')).sendKeys("Bonjour, ceci est un test de protractor");
 		
-		element(by.name('Submit')).click();
-	})
-	it('compleat complete form', function() {
-		browser.get('http://82.223.82.41:8080/');
+	// 	element(by.name('Submit')).click();
+	// })
+	// it('compleat complete form', function() {
+	// 	browser.get('http://82.223.82.41:8080/');
 
-		element(by.css('[href="/login"]')).click();
+	// 	element(by.css('[href="/login"]')).click();
 		
-		element(by.id('email')).sendKeys("vivien.pradelles@gmail.com");
-		element(by.id('password')).sendKeys("azerty");
+	// 	element(by.id('email')).sendKeys("vivien.pradelles@gmail.com");
+	// 	element(by.id('password')).sendKeys("azerty");
 		
-		element(by.name('Submit')).click();
+	// 	element(by.name('Submit')).click();
 
-		//expect(element(by.css('MessageTitle')).getText()).toEqual("Successful");
-	})
+	// 	//expect(element(by.css('MessageTitle')).getText()).toEqual("Successful");
+	// })
 });
