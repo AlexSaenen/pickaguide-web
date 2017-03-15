da454a816769366defc61202a100ee0caf09ee70
AlexSaenen <alexander.saenen@epitech.eu>
2017-03-14 18:27:29 -0700

    Added comments

--- client/components/views/Home.jsx
+++ client/components/views/Home.jsx
@@ -5,6 +5,15 @@ import { PanelList } from 'view/PanelList.jsx';
 import { Element } from 'layout/Element.jsx';
 import { Lister } from 'layout/Lister.jsx';
 
+// Lister est une liste seule a afficher au lieu d'un Panel (les elements sont libres)
+// PanelList est une liste dans un Panel
+// ModalList est une liste dans une Modal
+// Bien sur vous pouvez vous meme creer un Component Modal et manuellement y inserer une Liste,
+// ModalList et PanelList sont juste des shortcuts
+
+// listStyle => ListGrid (elements un a cote des autres) ou ListStack (elements de haut en bas, default)
+// wrapChildren => si true (default) la Liste va encapsuler chaque node HTML par Element, false signifie que
+// le user le fait manuellement, chaque fils HTML a au final besoin d'etre encapsule par Element
 
 const Home = () => {
   return (
