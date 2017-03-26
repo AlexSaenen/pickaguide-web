bcdfc68c9e86aabf28a05cae1a3b9009c2860b0a
AlexSaenen <alexander.saenen@epitech.eu>
2017-03-25 18:28:42 -0700

    Removed logs

--- client/components/framework/layout/elements/Picture.jsx
+++ client/components/framework/layout/elements/Picture.jsx
@@ -14,7 +14,6 @@ export class Picture extends PropsComponent {
   }
 
   render() {
-    console.log('Picture.render()');
     return (
       <div className="Picture">
         <img alt={this.state.pictureName} src={this.state.url} />
