2d2c38f313e71c92b43e23274dbcc1aae1cc8e7e
AlexSaenen <alexander.saenen@epitech.eu>
2017-04-25 17:54:26 -0700

    Server side rendering

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use('/assets', express.static(path.resolve(__dirname, './assets')));
 
-app.get('/', (request, response) => {
+app.get('/*', (request, response) => {
   response.sendFile(`${__dirname}/index.html`);
 });
 
