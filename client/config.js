42a31fd9b7ea8203b1602e861e0f4ed120d4e71d
AlexSaenen <alexander.saenen@epitech.eu>
2017-07-01 15:21:31 +0200

    Back to original

--- client/config.js
+++ client/config.js
@@ -1,7 +1,6 @@
 function getApiUrl() {
   if (process.env.ENVIRONMENT === 'production') {
-    return 'http://pickaguide.fr:3030';
-    // return 'https://pickaguide.fr:3000';
+    return 'https://pickaguide.fr:3000';
   } else if (process.env.ENVIRONMENT === 'staging') {
     return 'http://pickaguide.fr:3030';
   }
