96c405caf7a88f6ae87e861464289875dcb5c2fe
AlexSaenen <alexander.saenen@epitech.eu>
2017-07-05 11:06:46 +0200

    View(Guide) => replaced system of "all inputs and hide" to "only needed inputs" in Guide Form

--- client/components/modals/Guide.jsx
+++ client/components/modals/Guide.jsx
@@ -50,17 +50,20 @@ export class Guide extends StoreObserver {
 
   render() {
     const profile = this.state.profile || {};
-    const shouldDisplay = attribute => (attribute ? 'Hidden' : '');
+    const neededAttrs = ['firstName', 'lastName', 'phone', 'city', 'country'];
+    const attrNameMapping = {
+      firstName: 'First name',
+      lastName: 'Last name',
+    };
+
+    const inputs = neededAttrs
+      .filter(attr => !profile[attr])
+      .map((attr, index) => <TextInput key={index} value={profile[attr]} label={attr} placeholder={attrNameMapping[attr] || attr} required />);
 
     return (
       <ModalForm controller={this.ctrl} {...this.props} layoutStyle="LayoutDark Tight" modalStyle="Large">
         <Title>Become Guide</Title>
-        <TextInput className={shouldDisplay(profile.firstName)} value={profile.firstName} label="firstName" placeholder="First name" required />
-        <TextInput className={shouldDisplay(profile.lastName)} value={profile.lastName} label="lastName" placeholder="Last name" required />
-        <TextInput className={shouldDisplay(profile.phone)} value={profile.phone} label="phone" required />
-        <TextInput className={shouldDisplay(profile.city)} value={profile.city} label="city" required />
-        <TextInput className={shouldDisplay(profile.country)} value={profile.country} label="country" required />
-        <TextArea className={shouldDisplay(profile.description)} value={profile.description} label="description" required />
+        {inputs}
       </ModalForm>
     );
   }
