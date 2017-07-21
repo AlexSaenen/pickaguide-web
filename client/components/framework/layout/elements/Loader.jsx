bb1a557bc22d4cf0968b6fc436b27ca54b31d8ab
AlexSaenen <alexander.saenen@epitech.eu>
2017-07-21 18:40:02 +0200

    Center the Loader with MarginAuto

--- client/components/framework/layout/elements/Loader.jsx
+++ client/components/framework/layout/elements/Loader.jsx
@@ -9,7 +9,7 @@ export class Loader extends React.Component {
   render() {
     return (
       <div className="Loader">
-        <img role="presentation" src="/assets/images/loader.gif" width="30" height="30" />
+        <img className="MarginAuto" role="presentation" src="/assets/images/loader.gif" width="30" height="30" />
       </div>
     );
   }
