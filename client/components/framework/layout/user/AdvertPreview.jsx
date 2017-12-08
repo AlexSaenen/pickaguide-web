171823a23b167b90b86eab1f7452c7dada460ed7
Bozotek <sangoi.lucas@gmail.com>
2017-12-08 12:30:06 +0100

    improvment star system

--- client/components/framework/layout/user/AdvertPreview.jsx
+++ client/components/framework/layout/user/AdvertPreview.jsx
@@ -68,8 +68,10 @@ export class AdvertPreview extends React.Component {
 
         <div className="DescriptionSection Box" style={{ width: `${this.props.owner ? '65%' : '80%'} !important`, verticalAlign: 'top !important' }}>
           <div className="LineSpaced">
-            <div className="star-ratings-sprite"><span style={{ width: '80%' }} className="star-ratings-sprite-rating"></span></div>
-
+            <div className="star-ratings-css">
+              <div className="star-ratings-css-top" style={{ width: '12%' }}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+              <div className="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+            </div>
             <p className="Medium Bold Inline">{this.props.title}</p>
 
             <p className="Inline Italic">in</p>
